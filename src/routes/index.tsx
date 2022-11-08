4871a1421492b63bff3a0b0dae16f6153c317890
Olivier Gamache <olivier@glo.quebec>
2022-11-08 13:56:37 -0500

    feat: les demandes a nath

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,8 +11,6 @@ import { mainFeatures } from 'src/content/features'
 import { codeExamples } from 'src/content/examples'
 
 export default function() {
-  let ref: HTMLDivElement
-
   const [isScrolling, setIsScrolling] = createSignal(false)
 
   createEffect(() => {
@@ -44,7 +42,7 @@ export default function() {
   })
 
   return (
-    <div ref={ref!} class="full flex flex-col">
+    <div class="bg-gray-800 w-full flex flex-col">
       <nav class={`fixed w-full px-10 py-5 z-50 flex items-center text-gray-50 font-semibold transition-colors ${isScrolling() && 'bg-[#F34918] shadow-2xl'}`}>
         <p class="text-3xl">Burn</p>
         <ul class="ml-auto flex space-x-12 text-xl">
@@ -54,7 +52,7 @@ export default function() {
         </ul>
       </nav>
       <For each={Array.from({ length: 30 })} children={(_, i) => (
-          <div id={`star-${i()}`} class="absolute bg-[#EBC65D] rounded-full" />
+        <div id={`star-${i()}`} class="absolute bg-[#EBC65D] rounded-full" />
       )} />
       <div class="flex pt-4 sm:pt-10 flex-col sm:flex-row justify-center items-center sm:h-[70vh] bg-[#202124]">
         <div class="max-w-[650px]">
@@ -66,22 +64,24 @@ export default function() {
         </div>
       </div>
       <h2 class="bg-[#202124] w-full text-center py-10"><span class="bg-white p-1 rounded">features</span></h2>
-      <div class="grid sm:grid-cols-3 sm:px-36 gap-10 bg-gradient-to-b from-[#202124] to-gray-800">
-        <For each={mainFeatures} children={(feature) => (
-          <div class="flex justify-center cursor-default text-gray-300">
-            <div class="p-6 flex space-y-5 flex-col items-center w-[200px] text-center hover:bg-gray-50 hover:text-[#202124] hover:shadow-2xl hover:scale-105 rounded-lg transition-all">
-              <div class="text-5xl text-[#F34918]">{feature.icon}</div>
-              <h3 class="text-2xl font-bold">{feature.title}</h3>
-              <p class="text-lg">{
-                feature.description
-              }</p>
+      <div class="bg-gradient-to-b flex justify-center from-[#202124] to-gray-800">
+        <div class="grid sm:grid-cols-3 sm:gap-32">
+          <For each={mainFeatures} children={(feature) => (
+            <div class="flex justify-center cursor-default text-gray-300">
+              <div class="p-6 flex space-y-5 flex-col items-center w-[200px] text-center hover:bg-gray-50 hover:text-[#202124] hover:shadow-2xl hover:scale-105 rounded-lg transition-all">
+                <div class="text-5xl text-[#F34918]">{feature.icon}</div>
+                <h3 class="text-2xl font-bold">{feature.title}</h3>
+                <p class="text-lg">{
+                  feature.description
+                }</p>
+              </div>
             </div>
-          </div>
-        )} />
+          )} />
+        </div>
       </div>
-      <div class="bg-gray-800">
-        <h2 class="w-full text-center py-10 mt-20"><span class="bg-white p-1 rounded">examples</span></h2>
-        <div class="sm:px-36 items-center space-y-10 sm:space-y-32">
+      <div class="bg-gray-800 flex justify-center items-center flex-col">
+        <h2 class="w-full text-center py-10 pt-20"><span class="bg-white p-1 rounded">examples</span></h2>
+        <div class="max-w-7xl justify-center items-center space-y-10 sm:space-y-32 pb-14 border-b-2 border-gray-900">
           <For each={codeExamples} children={(example, i) => (
             <Motion.div
               initial={{ opacity: 0, x: (i() % 2 === 0 ? 1 : -1) * 20 }}
@@ -89,7 +89,7 @@ export default function() {
               transition={{ duration: 1 }}
               class={`flex flex-col-reverse p-4 sm:p-0 sm:space-x-10 items-center ${i() % 2 === 1 ? 'sm:flex-row-reverse' : 'sm:flex-row'}`}
             >
-              <pre class="border-2 border-gray-900 shadow rounded-lg w-full">
+              <pre class="border-2 border-gray-900 shadow rounded-lg w-80 md:w-full">
                 <code class="language-rust">
                   {example.code}
                 </code>
@@ -104,18 +104,72 @@ export default function() {
           )} />
         </div>
       </div>
-      <div class="w-full self-end text-4xl bg-gray-800 pt-40 space-y-10">
-        <ul class="w-full flex justify-center space-x-4">
-          <li><Outterlink src="https://twitter.com/nath_simard">
-            <div class="i-mdi-twitter text-blue-900 hover:text-blue-400 transition-colors" />
-          </Outterlink></li>
-          <li><Outterlink src="https://github.com/burn-rs/burn">
-            <div class="i-mdi-github hover:text-gray-50 transition-colors" />
-          </Outterlink></li>
-        </ul>
-        <div class="w-full text-center text-gray-400 text-sm">
-          built with ❤️ using <u><a href="https://bat.glo.quebec">🦇</a></u>
+      <div class="flex justify-center pt-10">
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-x-12 gap-y-6 md:gap-x-60 bg-gray-800 text-gray-500">
+          <ul>
+            <h3 class="uppercase mb-2 font-bold">Community</h3>
+            <li><Outterlink src="https://twitter.com/nath_simard">
+              <div class="flex items-center">
+                <div class="i-mdi-twitter text-xl" />
+                Twitter
+              </div>
+            </Outterlink></li>
+            <li><Outterlink src="https://github.com/burn-rs/burn">
+              <div class="flex items-center">
+                <div class="i-mdi-github text-xl" />
+                Github
+              </div>
+            </Outterlink></li>
+          </ul>
+          <ul>
+            <h3 class="uppercase mb-2 font-bold">Community</h3>
+            <li><Outterlink src="https://twitter.com/nath_simard">
+              <div class="flex items-center">
+                <div class="i-mdi-twitter text-xl" />
+                Twitter
+              </div>
+            </Outterlink></li>
+            <li><Outterlink src="https://github.com/burn-rs/burn">
+              <div class="flex items-center">
+                <div class="i-mdi-github text-xl" />
+                Github
+              </div>
+            </Outterlink></li>
+          </ul>
+          <ul>
+            <h3 class="uppercase mb-2 font-bold">Community</h3>
+            <li><Outterlink src="https://twitter.com/nath_simard">
+              <div class="flex items-center">
+                <div class="i-mdi-twitter text-xl" />
+                Twitter
+              </div>
+            </Outterlink></li>
+            <li><Outterlink src="https://github.com/burn-rs/burn">
+              <div class="flex items-center">
+                <div class="i-mdi-github text-xl" />
+                Github
+              </div>
+            </Outterlink></li>
+          </ul>
+          <ul>
+            <h3 class="uppercase mb-2 font-bold">Community</h3>
+            <li><Outterlink src="https://twitter.com/nath_simard">
+              <div class="flex items-center">
+                <div class="i-mdi-twitter text-xl" />
+                Twitter
+              </div>
+            </Outterlink></li>
+            <li><Outterlink src="https://github.com/burn-rs/burn">
+              <div class="flex items-center">
+                <div class="i-mdi-github text-xl" />
+                Github
+              </div>
+            </Outterlink></li>
+          </ul>
         </div>
+      </div>
+      <div class="w-full py-5 text-center text-gray-400 text-sm">
+        built with ❤️ using <u><a href="https://bat.glo.quebec">🦇</a></u>
       </div>
     </div>
   )
