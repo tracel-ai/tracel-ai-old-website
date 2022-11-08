536c227ed6918cb0238c0db9420e12b6678710f8
Olivier Gamache <olivier@glo.quebec>
2022-11-07 20:47:28 -0500

    feat: v2 with dark theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
-import launch from '@assets/launch.txt'
-import Animation from '@components/animation'
 import Outterlink from '@components/outterlink'
 
 // @ts-ignore
 import Prism from 'prismjs'
-import '@assets/prism-gruvbox-light.css'
+import '@assets/prism-theme.css'
 
 import logo from '@assets/logo.png'
+import burn from '@assets/burn.png'
 
 export default function() {
   const features = [
@@ -47,43 +46,10 @@ export default function() {
 
   })
 
-  return (
-    <div class="full flex flex-col">
-      <nav class="fixed w-full z-50 flex justify-end top-10 right-10">
-        <ul class="flex space-x-12 text-xl">
-          <li><Outterlink src="https://github.com/burn-rs/burn" label="GitHub" /></li>
-          <li><Outterlink src="https://docs.rs/burn/latest/burn" label="Docs" /></li>
-        </ul>
-      </nav>
-      <div class="flex items-center h-[70vh] bg-[#FFF0DF]">
-        <div>
-          <Animation src={launch} className="" />
-        </div>
-        <div class="mb-5">
-          <h1><img class="w-56" src={logo} /></h1>
-          <h2 class="text-red-300 font-bold text-normal w-full text-center">Burn Unstoppable Rusty Neurons</h2>
-        </div>
-      </div>
-      <h2 class="bg-[#FFF0DF] w-full text-center py-10"><span class="bg-white p-1 rounded">features</span></h2>
-      <div class="grid grid-cols-3 px-36 gap-10 bg-gradient-to-b from-[#FFF0DF] to-white">
-        <For each={features} children={(feature) => (
-          <div class="flex justify-center cursor-default">
-            <div class="p-6 flex space-y-5 flex-col items-center w-[200px] text-center hover:bg-white hover:shadow-2xl hover:scale-105 rounded-lg transition-all">
-              <div class="text-5xl text-[#F34918]">{feature.icon}</div>
-              <h3 class="text-2xl font-bold">{feature.title}</h3>
-              <p class="text-lg">{
-                feature.description
-              }</p>
-            </div>
-          </div>
-        )} />
-      </div>
-      <h2 class="w-full text-center py-10 mt-20"><span class="bg-white p-1 rounded">examples</span></h2>
-      <div class="px-36 items-center space-y-32">
-        <div class="flex space-x-10 items-center">
-          <pre class="border-2 rounded-lg w-full">
-            <code class="language-rust">
-              {`
+  const examples = [
+    {
+      code:
+        `
     use burn::tensor::{Tensor, Shape, Data};
     use burn::tensor::backend::{Backend, NdArrayBackend, TchBackend};
 
@@ -95,26 +61,13 @@ export default function() {
       my_func<NdArrayBackend<f32>>();
       my_func<TchBackend<f32>>();
     }
-          `}
-            </code>
-          </pre>
-          <div>
-            <h3 class="font-bold text-2xl">Tensor</h3>
-            <p>
-              The Tensor struct is at the core of the burn framework. It takes two generic parameters, the Backend and the number of dimensions D,
-            </p>
-          </div>
-        </div>
-        <div class="flex space-x-10 items-center">
-          <div>
-            <h3 class="font-bold text-2xl">Module</h3>
-            <p>
-              The Module derive let your create your own neural network module similar to PyTorch.
-            </p>
-          </div>
-          <pre class="border-2 rounded-lg w-full">
-            <code class="language-rust">
-              {`
+`,
+      description: 'The Tensor struct is at the core of the burn framework. It takes two generic parameters, the Backend and the number of dimensions D',
+      title: 'Tensor',
+    },
+    {
+      code:
+        `
     use burn::nn;
     use burn::module::{Param, Module};
     use burn::tensor::backend::Backend;
@@ -124,18 +77,71 @@ export default function() {
       my_param: Param<nn::Linear<B>>,
       repeat: usize,
     }
-          `}
-            </code>
-          </pre>
+`,
+      description: 'The Module derive let your create your own neural network module similar to PyTorch',
+      title: 'Module',
+    }
+
+  ]
+
+  return (
+    <div class="full flex flex-col">
+      <nav class="fixed w-full z-50 flex justify-end top-10 right-10 text-gray-50 font-semibold">
+        <ul class="flex space-x-12 text-xl">
+          <li><Outterlink src="https://github.com/burn-rs/burn">Github</Outterlink></li>
+          <li><Outterlink src="https://docs.rs/burn/latest/burn">Docs</Outterlink></li>
+        </ul>
+      </nav>
+      <div class="flex pt-10 justify-center items-center h-[70vh] bg-[#202124]">
+        <div class="max-w-[650px]">
+          <img src={burn} />
+        </div>
+        <div class="mb-5 pr-28">
+          <h1><img class="w-56" src={logo} /></h1>
+          <h2 class="text-red-300 font-bold text-normal w-full text-center">Burn Unstoppable Rusty Neurons</h2>
         </div>
       </div>
-      <div class="w-full self-end mt-40 space-y-10">
+      <h2 class="bg-[#202124] w-full text-center py-10"><span class="bg-white p-1 rounded">features</span></h2>
+      <div class="grid grid-cols-3 px-36 gap-10 bg-gradient-to-b from-[#202124] to-gray-800">
+        <For each={features} children={(feature) => (
+          <div class="flex justify-center cursor-default text-gray-300">
+            <div class="p-6 flex space-y-5 flex-col items-center w-[200px] text-center hover:bg-gray-50 hover:text-[#202124] hover:shadow-2xl hover:scale-105 rounded-lg transition-all">
+              <div class="text-5xl text-[#F34918]">{feature.icon}</div>
+              <h3 class="text-2xl font-bold">{feature.title}</h3>
+              <p class="text-lg">{
+                feature.description
+              }</p>
+            </div>
+          </div>
+        )} />
+      </div>
+      <div class="bg-gray-800">
+        <h2 class="w-full text-center py-10 mt-20"><span class="bg-white p-1 rounded">examples</span></h2>
+        <div class="px-36 items-center space-y-32">
+          <For each={examples} children={(example, i) => (
+            <div class={`flex space-x-10 items-center ${i() % 2 === 1 && 'flex-row-reverse'}`}>
+              <pre class="border-2 border-gray-900 shadow rounded-lg w-full">
+                <code class="language-rust">
+                  {example.code}
+                </code>
+              </pre>
+              <div>
+                <h3 class="font-black uppercase text-[#F34918] text-4xl">{example.title}</h3>
+                <p class="text-gray-50">
+                  {example.description}
+                </p>
+              </div>
+            </div>
+          )} />
+        </div>
+      </div>
+      <div class="w-full self-end text-4xl bg-gray-800 pt-40 space-y-10">
         <ul class="w-full flex justify-center space-x-4">
           <li><Outterlink src="twitter.com">
-            <div class="i-mdi-twitter text-xl"/>
+            <div class="i-mdi-twitter text-blue-900 hover:text-blue-400 transition-colors" />
           </Outterlink></li>
           <li><Outterlink src="github.com">
-            <div class="i-mdi-github text-xl"/>
+            <div class="i-mdi-github hover:text-gray-50 transition-colors" />
           </Outterlink></li>
         </ul>
         <div class="w-full text-center text-gray-400 text-sm">
