2cebfe6137b432d708b340a6b170c2010b237932
Olivier Gamache <olivier@glo.quebec>
2023-01-29 05:08:45 -0500

    feat: blog with route data

--- src/routes/blog/[id].tsx
+++ src/routes/blog/[id].tsx
@@ -3,37 +3,57 @@ import Layout from 'src/layout/page'
 import remarkParse from 'remark-parse'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkParseFrontmatter from 'remark-parse-frontmatter'
-import { useParams } from 'solid-start'
+import { createRouteData, RouteDataArgs, useRouteData } from 'solid-start'
 import { unified } from 'unified'
 import remarkHtml from 'remark-html'
 
+export function routeData({ params }: RouteDataArgs) {
+  return createRouteData(async () => {
+    try {
+      const url = new URL('https://burn-rs.github.io/content/blog/' + params.id + '.txt')
+      const response = await fetch(url.toString())
+      const markdown = await response.text()
+
+      const file = await unified()
+        .use(remarkParse)
+        .use(remarkFrontmatter, ['yaml', 'toml'])
+        .use(remarkParseFrontmatter)
+        .use(remarkHtml)
+        .process(markdown)
+
+      return {
+        meta: file.data.frontmatter,
+        value: String(file),
+      }
+    } catch (e) {
+      console.error(e)
+
+      return {
+        value: 404
+      }
+    }
+  })
+}
 
 const Blog = () => {
-  const [blog, setBlog] = createSignal('')
-  createEffect(async () => {
-    const params = useParams()
-
-    const response = await import('../../content/blog/' + params.id).then(m => m.default)
-
-    const file = unified()
-      .use(remarkParse)
-      .use(remarkFrontmatter, ['yaml', 'toml'])
-      .use(remarkParseFrontmatter)
-      .use(remarkHtml)
-      .processSync(response)
-    setBlog(String(file))
-    console.log(file.data.frontmatter)
-  })
+  const blog = useRouteData<typeof routeData>()
 
   return (
     <Layout>
       <div
         class="blog"
       >
-        <div innerHTML={blog()} />
-      </div>
+        <Suspense>
+          <div innerHTML={blog()?.value} />
+        </Suspense>
+        <Suspense>
+          <Show when={blog()?.meta?.author}>
+            <h2>{blog()?.meta?.author}</h2>
+          </Show>
+        </Suspense>
+      </div >
 
-    </Layout>
+    </Layout >
   )
 }
 
