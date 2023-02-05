a47d30f85b87291c73866e44d4431220ab34520f
nathaniel <nathaniel.simard.42@gmail.com>
2023-02-05 14:40:04 -0500

    Better scription

--- src/content/blogs.ts
+++ src/content/blogs.ts
@@ -1,7 +1,7 @@
 export const loremIpsum = {
   links: '/blog/lorem',
   title: 'Lorem',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis dolor volutpat sapien commodo interdum. Etiam bibendum ultrices luctus. Ut commodo egestas tincidunt. Nunc cursus ex ut varius pharetra. Etiam sit amet efficitur tortor. Sed porta imperdiet maximus. Suspendisse rutrum suscipit dignissim.',
   author: 'Lorem ipsum',
   publishedDate: '1234-03-02T05:17:32.000Z',
 }
