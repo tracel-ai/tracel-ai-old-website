a5d73a0284b662a0c252030e8d1482c3bca78abd
nathanielsimard <nathanielsimard@users.noreply.github.com>
2023-02-11 21:25:20 +0000

    deploy: 6329487e03bf257fcfb9b9d2086c46a91baae364

---
+++ analytics.js
@@ -0,0 +1,10 @@
+function gtag() {
+  if (!window.dataLayer) {
+    window.dataLayer = []
+  }
+
+  window.dataLayer.push(arguments)
+}
+
+gtag('js', new Date())
+gtag('config', 'G-S6CQMW5DNY')
---
+++ assets/_...404_-f102fdc5.js
@@ -0,0 +1,1 @@
+import{g as t,t as e}from"./entry-client-fe0a969e.js";const _=e("<div>404</div>",2);function m(){return t(_)}export{m as default};
