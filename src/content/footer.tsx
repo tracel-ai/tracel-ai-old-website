14dd023d4916fd1a047406520be4c2aa2d2726a2
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2023-02-21 20:37:39 -0500

    Fix: remove dead code and dupilcated github link + fix license link (#8)

--- src/content/footer.tsx
+++ src/content/footer.tsx
@@ -30,23 +30,7 @@ export const footerSections: FooterSection[] = [
     items: [
       { label: 'Documentation', href: 'https://docs.rs/burn' },
       { label: 'Crates.io', href: 'https://crates.io/crates/burn' },
-      { label: 'License', href: 'https://github.com/burn-rs/burn/blob/master/LICENSE' },
+      { label: 'License', href: 'https://github.com/burn-rs/burn#license' },
     ],
   },
 ]
-
-export const footer = {
-  examples: [
-    { label: 'MNIST', href: 'https://github.com/burn-rs/burn/tree/main/examples/mnist' },
-    { label: 'Text Classification', href: 'https://github.com/burn-rs/burn/tree/main/examples/text-classification' },
-  ],
-  community: [
-    { label: 'Github', href: 'https://github.com/burn-rs/burn', icon: 'i-mdi-github' },
-    { label: 'Discord', href: 'https://discord.gg/KcVGzmCcWj', icon: 'i-mdi-discord' },
-  ],
-  about: [
-    { label: 'Documentation', href: 'https://docs.rs/burn' },
-    { label: 'Crates.io', href: 'https://crates.io/crates/burn' },
-    { label: 'License', href: 'https://github.com/burn-rs/burn/blob/master/LICENSE' },
-  ],
-}
