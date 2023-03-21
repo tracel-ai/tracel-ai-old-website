e5c15948fd7ea9afa5c8888e72f4ecde5884c887
nathaniel <nathaniel.simard.42@gmail.com>
2023-03-20 20:47:12 -0400

    improve demo

--- src/routes/demo.tsx
+++ src/routes/demo.tsx
@@ -1,6 +1,33 @@
 import Layout from 'src/layout/page'
+import { createSignal } from 'solid-js';
 
 export default function() {
+  let [height, setHeight] = createSignal(615);
+
+  onMount(() => {
+    const resize = (doc: Document) => {
+      if (doc.body) {
+        const rect = doc.body.getBoundingClientRect();
+        if (rect && rect.height > 500) {
+          setHeight(rect.height + 40);
+        }
+      }
+    }
+
+    const iframe: any = document.getElementById('demo-iframe');
+    const doc: Document = iframe.contentDocument;
+    const win: Window = iframe.contentWindow;
+
+    win.addEventListener("resize", () => resize(doc));
+
+    // Initilize the iframe height
+    resize(doc);
+
+    // Adding some timeouts to make sure it is correctly initialized
+    setTimeout(() => resize(doc), 200);
+    setTimeout(() => resize(doc), 1000);
+  });
+
   return (
     <Layout>
       <div class="pt-[100px] px-6 flex justify-center ">
@@ -8,7 +35,7 @@ export default function() {
           <iframe 
             id="demo-iframe"
             class="max-w-[1500px] w-full bg-[#2a333e] rounded-lg px-6"
-            height="680px"
+            height={height()}
             src="/mnist_inference_web.html" 
           />
         </div>
