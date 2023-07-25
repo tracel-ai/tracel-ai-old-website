33c4b88597446de9842e20ea79412e8610f07390
nathaniel <nathaniel.simard.42@gmail.com>
2023-07-25 09:57:22 -0400

    Add multiple authors

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -2,47 +2,58 @@ import { Meta } from 'solid-start'
 import { BlogMetadata } from 'src/content/blogs'
 import { Component, JSX } from 'solid-js'
 
-const Blog: Component<{props: BlogMetadata, children: JSX.Element}> = ({props, children}) => {
-  let link = props.link;
+const Blog: Component<{ props: BlogMetadata; children: JSX.Element }> = ({
+  props,
+  children,
+}) => {
+  let link = props.link
   if (props.link[0] == '/') {
-    link = props.link.slice(1);
+    link = props.link.slice(1)
   }
 
   let current = ''
-  const paths = link.split('/').map(value => {
-    current = current + `/${value}`;
+  const paths = link.split('/').map((value) => {
+    current = current + `/${value}`
     return {
       href: current,
       name: value,
-    };
-  });
+    }
+  })
 
   return (
     <div class="flex justify-center w-full pt-20 bg-gradient-to-b from-[#202124] to-gray-800">
       <div class="w-full max-w-5xl mb-10 mx-3">
-        <Meta property='og:type' content='article' />
-        <Meta property='og:title' content={props.title} />
-        <Meta property='og:description' content={props.description} />
-        <Meta property='og:author' content={props.author} />
-        <Meta property='og:image' content={props.imageUrl} />
-        <Meta property='article:published_time' content={new Date(props.publishedDate).toISOString()} />
+        <Meta property="og:type" content="article" />
+        <Meta property="og:title" content={props.title} />
+        <Meta property="og:description" content={props.description} />
+        <Meta property="og:author" content={props.authors.join(', ')} />
+        <Meta property="og:image" content={props.imageUrl} />
+        <Meta
+          property="article:published_time"
+          content={new Date(props.publishedDate).toISOString()}
+        />
         <div class="mb-3">
           <p class="text-white font-bold text-xl px-2">
-            <a href="/" class="hover:text-[#edc567]">burn</a>
+            <a href="/" class="hover:text-[#edc567]">
+              burn
+            </a>
 
-            <For each={paths} children={(path) => (
-              <span>
-                <span>{' · '}</span>
-                <a href={path.href} class="hover:text-[#edc567]">{path.name}</a>
-              </span>
-            )} />
+            <For
+              each={paths}
+              children={(path) => (
+                <span>
+                  <span>{' · '}</span>
+                  <a href={path.href} class="hover:text-[#edc567]">
+                    {path.name}
+                  </a>
+                </span>
+              )}
+            />
           </p>
         </div>
         <article class="blog rounded-lg bg-white/5 pt-4">
           <div>
-            <h1 class="px-3 sm:px-8">
-              {props.title}
-            </h1>
+            <h1 class="px-3 sm:px-8">{props.title}</h1>
             <div class="px-3 pb-4 sm:px-8">
               <img
                 class="h-48 w-full rounded-lg mr-3 object-cover object-top"
@@ -53,17 +64,28 @@ const Blog: Component<{props: BlogMetadata, children: JSX.Element}> = ({props, c
             <div class="flex px-3 sm:px-8">
               <div class="flex">
                 <div class="h-5 w-5 i-mdi-clipboard-text-clock" />
-                <span class="px-2">{new Date(props.publishedDate).toDateString()}</span>
+                <span class="px-2">
+                  {new Date(props.publishedDate).toDateString()}
+                </span>
               </div>
-              <a class="pl-2 flex" href={props.authorLink} target="_blank">
-                <div class="h-5 w-5 i-mdi-account-edit" />
-                <span class="px-2">{props.author}</span>
-              </a>
+              <For
+                each={props.authors}
+                children={(author, index) => (
+                  <a
+                    class="pl-2 flex"
+                    href={props.authorsLinks.at(index as any)}
+                    target="_blank"
+                  >
+                    <div class="h-5 w-5 i-mdi-account-edit" />
+                    <span class="px-2">{author}</span>
+                  </a>
+                )}
+              />
             </div>
           </div>
           <div class="pb-4 px-3 sm:px-8">
             <div class="border-t-2 border-gray-900 my-6" />
-            { children }
+            {children}
           </div>
         </article>
       </div>
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,9 +7,10 @@ const Card: Component<BlogMetadata> = (props) => {
     <A href={props.link} class="flex justify-center max-w-5xl">
       <div class="py-4 w-full">
         <div class="w-full flex flex-col sm:flex-row bg-white/5 rounded-lg text-white hover:scale-110 transition-all hover:shadow-lg">
-          <img 
+          <img
             class="h-48 rounded-lg sm:rounded-l-lg sm:mr-3 object-cover"
-            src={props.imageUrl} alt={props.imageUrlAlt}
+            src={props.imageUrl}
+            alt={props.imageUrlAlt}
           />
           <div class="flex flex-col-reverse sm:flex-row p-3">
             <div class="">
@@ -19,11 +20,13 @@ const Card: Component<BlogMetadata> = (props) => {
               <div class="flex my-3">
                 <div class="flex text-sm">
                   <div class="h-5 w-5 i-mdi-clipboard-text-clock" />
-                  <span class="px-2">{new Date(props.publishedDate).toDateString()}</span>
+                  <span class="px-2">
+                    {new Date(props.publishedDate).toDateString()}
+                  </span>
                 </div>
                 <div class="flex text-sm px-2">
                   <div class="h-5 w-5 i-mdi-account-edit" />
-                  <span class="px-2 h-6">{props.author}</span>
+                  <span class="px-2 h-6">{props.authors.join(', ')}</span>
                 </div>
               </div>
               <p class="text-lg font-medium line-clamp-3">
