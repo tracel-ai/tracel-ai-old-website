556236fe2ff97e23132e00c6f41a0710c53ecac8
nathaniel <nathaniel.simard.42@gmail.com>
2024-02-01 16:12:34 -0500

    Update book & Docs

--- update-burn.sh
+++ update-burn.sh
@@ -4,7 +4,7 @@ echo "Cloning Burn"
 git clone https://github.com/tracel-ai/burn
 cd burn
 git pull
-git checkout v0.12.0
+git checkout v0.12.1
 
 echo "Updating Burn Book"
 cd burn-book
@@ -24,6 +24,8 @@ echo "Updating Burn Docs"
 rm -rf ../public/docs
 mkdir -p ../public/docs
 
-RUSTDOCFLAGS='--theme ../burn.css --default-theme burn' cargo doc --no-deps
+# We only build the documentation for the burn crate, which redistributes the other crates.
+# Otherwise we have duplicated documentation.
+RUSTDOCFLAGS='--theme ../burn.css --default-theme burn' cargo doc --package burn --no-deps --no-default-features --features doc --release
 mv target/doc/* ../public/docs/
 cp ../burn.css ../public/docs/static.files/
